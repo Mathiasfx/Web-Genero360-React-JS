2a764738b288e3d13a66466090074a85230c22e2
Mathias Pereira <mathias_fx@yahoo.com.ar>
2022-10-13 01:53:30 -0300

    agregado de botones de descargas en el concurso

--- src/components/Concurso.js
+++ src/components/Concurso.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { SectionConcurso } from "../Styles/Main";
 import flyer from "../images/concurso.png";
+import bases from "../assets/Contale al mundo lo que ves - Bases y condiciones.pdf";
+import permisos from "../assets/Autorizacion_genero 360.doc";
 
 const Concurso = () => {
   return (
@@ -11,7 +13,15 @@ const Concurso = () => {
           alt="Imagen Concurso"
           className="imgConcurso img-fluid"
         />
-        <p>*Base y condiciones disponibles a partir del 12 de Octubre</p>
+        <div className="contenedorBotonera">
+          <p>*Descarga las bases y condiciones del concurso </p>
+          <a href={bases}>
+            <button className="btn btn-primary">Bases y Condiciones</button>
+          </a>
+          <a href={permisos}>
+            <button className="btn  btn-primary ml-3">Permisos</button>
+          </a>
+        </div>
       </div>
     </SectionConcurso>
   );
